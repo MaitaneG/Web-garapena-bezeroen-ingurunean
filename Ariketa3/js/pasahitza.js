a0495794b2e866f5189f748c16882da6e8b1a1f4
MaitaneG <Maitane>
2022-09-15 19:19:38 +0200

    3. Ariketa bukatu

--- Ariketa3/js/pasahitza.js
+++ Ariketa3/js/pasahitza.js
@@ -1,12 +1,19 @@
-function pashitzaKonprobatu() {
+function pasahitzaEskatu() {
   let pasahitzaOndo = false;
   while (!pasahitzaOndo) {
-    pasahitzakEskatu
+    pasahitzaOndo = pasahitzakKonprobatu();
   }
 }
 
-function pasahitzakEskatu() {
+function pasahitzakKonprobatu() {
   let pasahitza1 = prompt("Pasahitza sartu");
   let pasahitza2 = prompt("Pasahitza berriro sartu");
-}
 
+  if (pasahitza1 == pasahitza2) {
+    alert("Ongi etorri");
+    return true;
+  } else {
+    alert("Pasahitza okerrra");
+    return false;
+  }
+}
