529fdc4a0f7192c7079bd84ec45883cfc808ebc9
Maitane <gallastegui.maitane@uni.eus>
2022-09-21 14:21:18 +0200

    DOM ariketa

--- Adibideak/Ariketa 1/js/kamisetak.js
+++ Adibideak/Ariketa 1/js/kamisetak.js
@@ -2,13 +2,17 @@ function aurrera(){
     let linkAurrera = document.getElementById("aurrera");
     linkAurrera.onclick = function(){
         let kamisetaZbk= document.getElementById("kamisetaZbk");
+        let argazkia=document.getElementById("argazkia")
 
         if(kamisetaZbk.textContent == "Camiseta 6"){
             kamisetaZbk.textContent= "Camiseta 9";
+            argazkia="img/9jpg"
         }else if(kamisetaZbk.textContent == "Camiseta 9"){
             kamisetaZbk.textContent= "Camiseta 11";
+            argazkia="img/11jpg"
         }else{
             kamisetaZbk.textContent= "Camiseta 6";
+            argazkia="img/6jpg"
         }
     }
 }
