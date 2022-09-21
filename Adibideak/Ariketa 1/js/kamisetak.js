9be0205118060f0c8c262796c4b79991f4fe0e9d
MaitaneG <Maitane>
2022-09-21 17:06:50 +0200

    Ariketa 1 diseinua falta da

--- Adibideak/Ariketa 1/js/kamisetak.js
+++ Adibideak/Ariketa 1/js/kamisetak.js
@@ -6,17 +6,32 @@ function aurrera(){
 
         if(kamisetaZbk.textContent == "Camiseta 6"){
             kamisetaZbk.textContent= "Camiseta 9";
-            argazkia="img/9jpg"
+            argazkia.src="img/9.jpg"
         }else if(kamisetaZbk.textContent == "Camiseta 9"){
             kamisetaZbk.textContent= "Camiseta 11";
-            argazkia="img/11jpg"
+            argazkia.src="img/11.jpg"
         }else{
             kamisetaZbk.textContent= "Camiseta 6";
-            argazkia="img/6jpg"
+            argazkia.src="img/6.jpg"
         }
     }
 }
 
 function atzera(){
+    let linkAurrera = document.getElementById("atzera");
+    linkAurrera.onclick = function(){
+        let kamisetaZbk= document.getElementById("kamisetaZbk");
+        let argazkia=document.getElementById("argazkia")
 
+        if(kamisetaZbk.textContent == "Camiseta 11"){
+            kamisetaZbk.textContent= "Camiseta 9";
+            argazkia.src="img/9.jpg"
+        }else if(kamisetaZbk.textContent == "Camiseta 9"){
+            kamisetaZbk.textContent= "Camiseta 6";
+            argazkia.src="img/6.jpg"
+        }else{
+            kamisetaZbk.textContent= "Camiseta 11";
+            argazkia.src="img/11.jpg"
+        }
+    }
 }
