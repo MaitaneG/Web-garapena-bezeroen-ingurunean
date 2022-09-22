9491f0b80040bc836b6e9c1cdea4f76ab5c85d63
MaitaneG <Maitane>
2022-09-22 20:11:55 +0200

    Ariketa 2 bukatuta

--- Adibideak/Ariketa 2/js/form.js
+++ Adibideak/Ariketa 2/js/form.js
@@ -1,16 +1,20 @@
 function bidaliInfor() {
+  ezabatu();
+
   let radioAnderea = document.getElementById("anderea");
 
+  let gender = "Jauna";
+
   if (radioAnderea.checked) {
-    let gender = "Jauna";
-  } else {
-    let gender = "anderea";
+    gender = "Anderea";
   }
 
   let izena = document.getElementById("izena");
   let abizena = document.getElementById("abizena");
   let helbidea = document.getElementById("helbidea");
   let herrialdea = document.getElementById("herrialdea");
+  let liburuak = document.getElementById("liburuak");
+  let pelikulak = document.getElementById("pelikulak");
 
   if (
     izena.value.trim() == "" ||
@@ -19,24 +23,48 @@ function bidaliInfor() {
     herrialdea.value.trim() == ""
   ) {
     alert("Datu guztiak bete behar dira");
-  } else {
+  } else if (liburuak.checked || pelikulak.checked) {
     let izenaIda = document.getElementById("sartuIzena");
     let abizenaIda = document.getElementById("sartuAbizena");
     let helbideaIda = document.getElementById("sartuHelbidea");
     let herrialdeaIda = document.getElementById("sartuHerrialdea");
+    let generoaIda = document.getElementById("sartuGeneroa");
 
-    izenaIda.textContent = "Izena: "+izena.value.trim();
-    abizenaIda.textContent = "Abizena: "+abizena.value.trim();
-    helbideaIda.textContent = "Helbidea: "+helbidea.value.trim();
-    herrialdeaIda.textContent = "Herrialdea: "+herrialdea.value.trim();
+    let gustokoenaIda = document.getElementById("sartuGustokoena");
+
+    if (liburuak.checked && pelikulak.checked) {
+      gustokoenaIda.textContent = "Gustokoenak: Liburuak eta Pelikulak";
+    } else if (liburuak.checked) {
+      gustokoenaIda.textContent = "Gustokoena: Liburuak";
+    } else {
+      gustokoenaIda.textContent = "Gustokoena: Pelikulak";
+    }
+
+    izenaIda.textContent = "Izena: " + izena.value.trim();
+    abizenaIda.textContent = "Abizena: " + abizena.value.trim();
+    helbideaIda.textContent = "Helbidea: " + helbidea.value.trim();
+    herrialdeaIda.textContent = "Herrialdea: " + herrialdea.value.trim();
+    generoaIda.textContent = "Generoa: " + gender;
+  } else {
+    alert("Gutxienez gustoko bat aukeratu behar duzu.");
   }
   return false;
 }
 
-function ezeztatu() {
-  let argazkia = document.getElementById("arg");
-  let radioJauna = document.getElementById("jauna");
-  let radioAnderea = document.getElementById("anderea");
+function ezabatu() {
+  let izenaIda = document.getElementById("sartuIzena");
+  let abizenaIda = document.getElementById("sartuAbizena");
+  let helbideaIda = document.getElementById("sartuHelbidea");
+  let herrialdeaIda = document.getElementById("sartuHerrialdea");
+  let generoaIda = document.getElementById("sartuGeneroa");
+  let gustokoenaIda = document.getElementById("sartuGustokoena");
+
+  izenaIda.textContent="";
+  abizenaIda.textContent="";
+  helbideaIda.textContent="";
+  herrialdeaIda.textContent="";
+  generoaIda.textContent="";
+  gustokoenaIda.textContent="";
 }
 
 function argazkiaAldatu() {
