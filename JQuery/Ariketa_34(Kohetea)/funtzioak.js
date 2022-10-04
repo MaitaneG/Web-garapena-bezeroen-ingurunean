c1005c5206270e7b244227cc791557407db44f22
Maitane <gallastegui.maitane@uni.eus>
2022-10-04 10:04:31 +0200

    Animate finish gehitu, prozesua direktamente bukatzeko eta ez stack-etzeko

--- JQuery/Ariketa_34(Kohetea)/funtzioak.js
+++ JQuery/Ariketa_34(Kohetea)/funtzioak.js
@@ -3,34 +3,34 @@ $(document).ready(function () {
     switch (e.key) {
       case "ArrowLeft":
         if(e.ctrlKey){
-            $("img").animate({left: "-=35px", top: "-=35"}, 1000);
+            $("img").finish().animate({left: "-=35px", top: "-=35"}, 1000);
             $("img").css({transform: 'rotate('+315+'deg)'})
         }else if(e.shiftKey){
-            $("img").animate({left: "-=35",top: "+=35" }, 1000);
+            $("img").finish().animate({left: "-=35",top: "+=35" }, 1000);
             $("img").css({transform: 'rotate('+225+'deg)'})
         }else{
-            $("img").animate({left: "-=50px" }, 1000);
+            $("img").finish().animate({left: "-=50px" }, 1000);
             $("img").css({transform: 'rotate('+270+'deg)'})
         }
         break;
       case "ArrowRight":
         if(e.ctrlKey){
-            $("img").animate({left: "+=35px", top: "-=35"}, 1000);
+            $("img").finish().animate({left: "+=35px", top: "-=35"}, 1000);
             $("img").css({transform: 'rotate('+45+'deg)'})
         }else if(e.shiftKey){
-            $("img").animate({left: "+=35",top: "+=35" }, 1000);
+            $("img").finish().animate({left: "+=35",top: "+=35" }, 1000);
             $("img").css({transform: 'rotate('+135+'deg)'})
         }else{
-            $("img").animate({left:"+=50px"}, 1000)
+            $("img").finish().animate({left:"+=50px"}, 1000)
             $("img").css({transform: 'rotate('+90+'deg)'})
         }
         break;
       case "ArrowUp":
-        $("img").animate({top:"-=50px"}, 1000)
+        $("img").finish().animate({top:"-=50px"}, 1000)
         $("img").css({transform: 'rotate('+0+'deg)'})
         break;
       case "ArrowDown":
-        $("img").animate({top:"+=50px"}, 1000)
+        $("img").finish().animate({top:"+=50px"}, 1000)
         $("img").css({transform: 'rotate('+180+'deg)'})
         break;
     }
