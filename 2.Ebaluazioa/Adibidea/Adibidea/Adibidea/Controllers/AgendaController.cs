31f9aaa37e71fd83e4f603251be7af35bae39426
Maitane <gallastegui.maitane@uni.eus>
2022-11-22 08:02:27 +0100

    Bukatuta adibidea

--- 2.Ebaluazioa/Adibidea/Adibidea/Adibidea/Controllers/AgendaController.cs
+++ 2.Ebaluazioa/Adibidea/Adibidea/Adibidea/Controllers/AgendaController.cs
@@ -9,13 +9,24 @@ namespace Adibidea.Controllers
         // GET: AgendaController
         public ActionResult Index()
         {
-            return View();
+            List<Kontaktua> lstKontaktuak = new List<Kontaktua>();
+            Kontaktua kontaktua1 = new Kontaktua("13456765", "izaskun", "Kortabitarte");
+            kontaktua1.Emaila = "ikortabitarte@uni.eus";
+            lstKontaktuak.Add(kontaktua1);
+            Kontaktua kontaktua2 = new Kontaktua("13452345", "pepe", "Fernandez");
+            kontaktua2.Emaila = "pfernandez@uni.eus";
+            lstKontaktuak.Add(kontaktua2);
+            Kontaktua kontaktua3 = new Kontaktua("14567874", "jon", "Perez");
+            kontaktua3.Emaila = "jperez@uni.eus";
+            lstKontaktuak.Add(kontaktua3);
+            return View(lstKontaktuak);
         }
 
         // GET: AgendaController/Details/5
         public ActionResult Details() { 
             Kontaktua kontaktua = new Kontaktua("13456765", "izaskun", "Kortabitarte"); 
-            kontaktua.Emaila = "ikortabitarte@uni.eus"; return View(kontaktua); 
+            kontaktua.Emaila = "ikortabitarte@uni.eus"; 
+            return View(kontaktua); 
         }
 
         // GET: AgendaController/Create
