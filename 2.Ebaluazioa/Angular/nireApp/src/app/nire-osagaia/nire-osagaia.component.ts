17c3dfa451924e6ff3f4d9d1d72797abafc602af
Maitane <gallastegui.maitane@uni.eus>
2022-11-24 13:34:33 +0100

    Angular eta Ionic

--- 2.Ebaluazioa/Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
+++ 2.Ebaluazioa/Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -12,4 +12,19 @@ export class NireOsagaiaComponent {
     generoa: 'gizona',
     irudia: './assets/img/Vilma.jpg'
     };
+
+    gehituLikes(langilea: any) { langilea.likes++; }
+
+    langileak = [ 
+      { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, likes:0, lanean:true }, 
+      { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, likes:0, lanean:true }, 
+      { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, likes:0, lanean:false }, 
+      { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, likes:0, lanean:false, }, 
+    ];
+
+    Title="Langileen zerrenda"; getTitle() { 
+      return this.Title; }
+
+    rowLimit = 3;
+    myText = "Nire izena: <h1>Izaskun</h1> da";
 }
